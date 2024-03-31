4608f4bcc095c37200392684ee2b2ca7b6b914ea
Adrian <142854436+withyso@users.noreply.github.com>
2024-03-31 15:37:53 +0000

    randomSuit function created

--- src/app.js
+++ src/app.js
@@ -8,4 +8,15 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  console.log(randomSuit());
 };
+
+function randomSuit() {
+  let suitGroup = ["♦", "♥", "♠", "♣"];
+  /*for (let iterator = 0; iterator < suitGroup.length; iterator++) {
+    console.log(suitGroup[iterator]);
+  }*/
+
+  let randomIcon = Math.floor(Math.random() * suitGroup.length);
+  return suitGroup[randomIcon];
+}
